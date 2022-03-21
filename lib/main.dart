3e403352fd87e9f793dd9ec5d65760126060bd93
shiiishiiir <shiiiishiiiir@gmail.com>
2022-03-22 01:30:00 +0600

    Sign In Email Auth Sign Up

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:firebase/ui/screens/SignInPage.dart';
 import 'package:firebase/ui/screens/SignUpPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/ui/screens/SignInPage.dart
+++ lib/ui/screens/SignInPage.dart
@@ -9,7 +9,7 @@ class SignInPagee extends StatefulWidget {
 }
 
 class _SignInPageeState extends State<SignInPagee> {
-  final _formKey= GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   bool isLogIn = false;
 
@@ -25,50 +25,115 @@ class _SignInPageeState extends State<SignInPagee> {
     }
   }
 
-
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text("FireBase Everything"),
+          title: Text("FireBase Email Sign In"),
           centerTitle: true,
         ),
         body: Center(
-          child: ListView(
-            children: [
-              Column(
-                children: [
-                  Text("Sign In"),
-                  Form(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Form(
                     key: _formKey,
                     child: Column(
                       children: [
+                        isLogIn
+                            ? TextFormField(
+                          key: ValueKey("Username"),
+                          decoration: InputDecoration(
+                            hintText: "Enter Your Username",
+                          ),
+                          validator: (value) {
+                            if (value.toString().length < 3) {
+                              return "Username is Small";
+                            } else {
+                              return null;
+                            }
+                          },
+                          onSaved: (value) {
+                            setState(() {
+                              username = value!;
+                            });
+                          },
+                        )
+                            : Container(),
                         TextFormField(
+                          key: ValueKey("Email"),
                           keyboardType: TextInputType.emailAddress,
                           decoration: InputDecoration(
                             labelText: "Enter Email or Phone Number",
                           ),
+                          validator: (value) {
+                            if (!(value.toString().contains("@"))) {
+                              return "Invalid Email";
+                            } else {
+                              return null;
+                            }
+                          },
+                          onSaved: (value) {
+                            setState(() {
+                              email = value!;
+                            });
+                          },
                         ),
                         TextFormField(
                           obscureText: true,
+                          key: ValueKey("Password"),
                           keyboardType: TextInputType.visiblePassword,
                           decoration: InputDecoration(
                             labelText: "Password",
                           ),
+                          validator: (value) {
+                            if (value.toString().length < 6) {
+                              return "Password is Small";
+                            } else {
+                              return null;
+                            }
+                          },
+                          onSaved: (value) {
+                            setState(() {
+                              password = value!;
+                            });
+                          },
                         ),
-
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: _handleSignUpData,
+                            child: isLogIn ? Text("Sign Up") : Text("Log In"),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              isLogIn = !isLogIn;
+                            });
+                          },
+                          child: isLogIn
+                              ? Text("Already SignUp? LogIn")
+                              : Text("Don't Have an Account? SignUp"),
+                        ),
                       ],
                     ),
                   ),
-                ],
-              )
-            ] ,
+                ),
+              ],
+            ),
           ),
         ),
       ),
